31bc0a2d6d45332631a58549d6c3394c89ab4387
juandpt03 <juandpt03@gmail.com>
2023-06-19 22:27:55 -0500

    first commit

---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_app/presentation/blocs/notifications/notifications_bloc.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: context.select((NotificationsBloc notifications) =>
+            Text('${notifications.state.status}')),
+        actions: [
+          IconButton(
+              onPressed: () {
+                context.read<NotificationsBloc>().requestPermission();
+                
+              },
+              icon: const Icon(Icons.settings))
+        ],
+      ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('HomeScreen');
+  }
+}
